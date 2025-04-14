f3c5b92239757a7467333a670a72d7bf64559b2c
ThibaDaniil <122720119+ThibaDaniil@users.noreply.github.com>
2025-04-14 20:31:42 +0400

    start

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hellow world")
+# Импортируем необходимые классы.
+import logging
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from config import BOT_TOKEN
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def start(update, context):
+    await update.message.reply_text("Привет, я бот помошник по Viki")
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler("/start", start))
+
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
